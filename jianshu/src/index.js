3f5e6f0a7f23f3832f7c2b99c1dc37d8ee32afae
yigeqi <firefly126@126.com>
2019-07-24 22:41:35 +0800

    create-react-app初始化项目，增加styled-components和全局样式reset.css内容

---
+++ jianshu/src/index.js
@@ -0,0 +1,9 @@
+import React, {
+	Fragment
+} from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import GlobalStyle from './style.js';
+
+
+ReactDOM.render(<Fragment><GlobalStyle/><App /></Fragment>, document.getElementById('root'));
