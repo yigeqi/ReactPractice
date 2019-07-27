69424e36ebda91f9374ff5ff92774e35eff570bb
yigeqi <firefly126@126.com>
2019-07-27 09:01:49 +0800

    新建actionCreators.js和constans.js用来分离action和actionTypes

---
+++ jianshu/src/common/Header/store/actionCreators.js
@@ -0,0 +1,9 @@
+import * as constans from './constans'
+
+export const searchInputFocus = ()=>({
+	type:constans.SEARCH_INPUT_FOCUS
+})
+
+export const searchInputBlur = ()=>({
+	type:constans.SEARCH_INPUT_BLUR
+})
---
+++ jianshu/src/common/Header/store/constans.js
@@ -0,0 +1,2 @@
+export const SEARCH_INPUT_FOCUS='header.search_input_focus';
+export const SEARCH_INPUT_BLUR='header.search_input_blur';
