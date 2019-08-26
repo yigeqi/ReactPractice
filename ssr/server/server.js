0c809e0f7e2f1147b366d83baa8000740246962d
yigeqi <firefly126@126.com>
2019-08-26 11:12:13 +0800

    server api

--- ssr/server/server.js
+++ ssr/server/server.js
@@ -1,15 +1,30 @@
 const ReactDOMServer = require('react-dom/server')
 const express = require('express')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 const favicon = require('serve-favicon')
 const fs = require('fs')
 const path = require('path')
 
 const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+// 把json格式和表单形式的post请求的数据都可以通过req.body取得
+app.use(session({
+  maxAge: 10 * 60 * 1000,
+  name: 'tid',
+  resave: false,
+  saveUninitialized: false,
+  secret: 'red apple'
+}))
 app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
 const isDev = process.env.NODE_ENV === 'development'
-
 console.log('isDev:', isDev)
+
+app.use('/api/user', require('./util/handle-login'))
+app.use('/api', require('./util/proxy'))
+
 if (!isDev) {
   // 生产模式下，直接使用build生成的dist文件夹里的index.html和js文件
   const template = fs.readFileSync(path.join(__dirname,'../dist/index.html'), 'utf8')
---
+++ ssr/server/util/proxy.js
@@ -0,0 +1,40 @@
+const axios = require('axios')
+
+const baseUrl = 'https://cnodejs.org/api/v1'
+
+module.exports = (req, res, next) => {
+  // 假定需要login后才能访问的接口都会在query里添加needAccessToken=true
+  const user = req.session.user || {}
+  const needAccessToken = req.query.needAccessToken
+  if (needAccessToken && !user.accessToken) {
+    res.status(401).send({
+      success: false,
+      msg: 'need login'
+    })
+  }
+  // 正式发请求前再把query里的needAccessToken删掉
+  const query = Object.assign({}, req.query)
+  if (query.needAccessToken) delete query.needAccessToken
+  axios(`${baseUrl}${req.path}`, {
+    method: req.method,
+    params: query,
+    data: Object.assign({}, req.body, {
+      accesstoken: user.accessToken
+    })
+  }).then(resp => {
+    if (resp.status === 200) {
+      res.send(resp.data)
+    } else {
+      res.status(resp.status).send(resp.data)
+    }
+  }).catch(err => {
+    if (err.response) {
+      res.status(500).send(err.response.data)
+    } else {
+      res.status(500).send({
+        success: false,
+        msg: 'unknown error.'
+      })
+    }
+  })
+}
